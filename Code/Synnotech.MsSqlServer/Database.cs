2339c34baccaffe0382a526b9fa3a3cc0f18c17d
Kenny Pflug <kenny.pflug@live.de>
2021-04-18 07:17:04 +0200

    implemented database helper methods

---
+++ Code/Synnotech.MsSqlServer/Database.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+using Light.GuardClauses;
+using Microsoft.Data.SqlClient;
+
+namespace Synnotech.MsSqlServer
+{
+    /// <summary>
+    /// Provides helper methods for creating and dropping MS SQL databases.
+    /// </summary>
+    public static class Database
+    {
+        /// <summary>
+        /// Tries to create the database the specified connection string points to.
+        /// This method will connect to the "master" database of the target
+        /// SQL server to do this - please ensure that the credentials in the connection string
+        /// have enough privileges to perform this operation.
+        /// </summary>
+        /// <param name="connectionString">The connection string that identifies the target database.</param>
+        /// <returns>True when the database was dropped, otherwise false.</returns>
+        /// <exception cref="KeyNotFoundException">Invalid key name within the connection string.</exception>
+        /// <exception cref="FormatException">Invalid value within the connection string (specifically, when a Boolean or numeric value was expected but not supplied).</exception>
+        /// <exception cref="ArgumentException">The supplied connectionString is not valid.</exception>
+        /// <exception cref="SqlException">Thrown when the connection to the master database fails or when the command fails to execute.</exception>
+        public static async Task<bool> TryCreateDatabase(string connectionString)
+        {
+            var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+            DatabaseName targetDatabaseName = connectionStringBuilder.InitialCatalog;
+            connectionStringBuilder.InitialCatalog = "master";
+
+#if NETSTANDARD2_0
+            using var connectionToMaster =
+#else
+            await using var connectionToMaster =
+#endif
+                new SqlConnection(connectionStringBuilder.ConnectionString);
+
+            await connectionToMaster.OpenAsync();
+            return await connectionToMaster.TryCreateDatabaseAsync(targetDatabaseName);
+        }
+
+        /// <summary>
+        /// Tries to drop the database the specified connection string points to.
+        /// This method will connect to the "master" database of the target
+        /// SQL server to do this - please ensure that the credentials in the connection string
+        /// have enough privileges to perform this operation.
+        /// </summary>
+        /// <param name="connectionString">The connection string that identifies the target database.</param>
+        /// <returns>True when the database was dropped, otherwise false.</returns>
+        /// <exception cref="KeyNotFoundException">Invalid key name within the connection string.</exception>
+        /// <exception cref="FormatException">Invalid value within the connection string (specifically, when a Boolean or numeric value was expected but not supplied).</exception>
+        /// <exception cref="ArgumentException">The supplied connectionString is not valid.</exception>
+        /// <exception cref="SqlException">Thrown when the connection to the master database fails or when the command fails to execute.</exception>
+        public static async Task<bool> TryDropDatabase(string connectionString)
+        {
+            var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+            DatabaseName targetDatabaseName = connectionStringBuilder.InitialCatalog;
+            connectionStringBuilder.InitialCatalog = "master";
+#if NETSTANDARD2_0
+            using var connectionToMaster =
+#else
+            await using var connectionToMaster =
+#endif
+                new SqlConnection(connectionStringBuilder.ConnectionString);
+
+            await connectionToMaster.OpenAsync();
+            await connectionToMaster.KillAllDatabaseConnectionsAsync(targetDatabaseName);
+            return await connectionToMaster.TryDropDatabaseAsync(targetDatabaseName);
+        }
+
+        /// <summary>
+        /// Creates the database for the specified connection string. If it already exists, the
+        /// database will be dropped and recreated. Connections to the existing database
+        /// will be terminated. This method will connect to the "master" database of the target
+        /// SQL server to do this - please ensure that the credentials in the connection string
+        /// have enough privileges to perform this operation.
+        /// </summary>
+        /// <param name="connectionString">The connection string that identifies the target database.</param>
+        /// <exception cref="KeyNotFoundException">Invalid key name within the connection string.</exception>
+        /// <exception cref="FormatException">Invalid value within the connection string (specifically, when a Boolean or numeric value was expected but not supplied).</exception>
+        /// <exception cref="ArgumentException">The supplied connectionString is not valid.</exception>
+        /// <exception cref="SqlException">Thrown when the connection to the master database fails.</exception>
+        public static async Task DropAndCreateDatabaseAsync(string connectionString)
+        {
+            var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+            DatabaseName targetDatabaseName = connectionStringBuilder.InitialCatalog;
+            connectionStringBuilder.InitialCatalog = "master";
+#if NETSTANDARD2_0
+            using var connectionToMaster =
+#else
+            await using var connectionToMaster =
+#endif
+                new SqlConnection(connectionStringBuilder.ConnectionString);
+
+            await connectionToMaster.OpenAsync();
+            await connectionToMaster.KillAllDatabaseConnectionsAsync(targetDatabaseName);
+            await connectionToMaster.DropAndCreateDatabaseAsync(targetDatabaseName);
+        }
+
+        /// <summary>
+        /// Execute a T-SQL command (non-query) that kills all active connections to the database
+        /// with the specified name. It is safe to run this command when the target database does
+        /// not exist.
+        /// </summary>
+        /// <param name="connectionToMaster">
+        /// The SQL connection that will be used to execute the command.
+        /// It must target the master database of a SQL server.
+        /// </param>
+        /// <param name="databaseName">The name of the target database.</param>
+        /// <param name="cancellationToken">The cancellation instruction (optional).</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="connectionToMaster"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="databaseName"/> is the default instance.</exception>
+        /// <exception cref="SqlException">Thrown when the command fails to execute.</exception>
+        public static async Task<bool> KillAllDatabaseConnectionsAsync(this SqlConnection connectionToMaster, DatabaseName databaseName, CancellationToken cancellationToken = default)
+        {
+            connectionToMaster.MustNotBeNull(nameof(connectionToMaster));
+            databaseName.MustNotBeDefault(nameof(databaseName));
+
+#if NETSTANDARD2_0
+            using var command = connectionToMaster.CreateCommand();
+#else
+            await using var command = connectionToMaster.CreateCommand();
+#endif
+
+            command.CommandType = CommandType.Text;
+            command.CommandText = $@"
+DECLARE @kill varchar(1000) = '';
+SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), session_id) + ';'
+FROM sys.dm_exec_sessions
+WHERE database_id = db_id('{databaseName}');
+
+EXEC(@kill);
+";
+            var result = await command.ExecuteNonQueryAsync(cancellationToken);
+            return result > 0;
+        }
+
+        /// <summary>
+        /// Execute a T-SQL command (non-query) that creates a new database. If the database
+        /// already exists, it will be dropped and recreated.
+        /// </summary>
+        /// <param name="connectionToMaster">
+        /// The SQL connection that will be used to execute the command.
+        /// It must target the master database of a SQL server.
+        /// </param>
+        /// <param name="databaseName">The name of the target database.</param>
+        /// <param name="cancellationToken">The cancellation instruction (optional).</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="connectionToMaster"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="databaseName"/> is the default instance.</exception>
+        /// <exception cref="SqlException">Thrown when the command fails to execute.</exception>
+        public static async Task<bool> DropAndCreateDatabaseAsync(this SqlConnection connectionToMaster, DatabaseName databaseName, CancellationToken cancellationToken = default)
+        {
+            connectionToMaster.MustNotBeNull(nameof(connectionToMaster));
+            databaseName.MustNotBeDefault(nameof(databaseName));
+
+#if NETSTANDARD2_0
+            using var command = connectionToMaster.CreateCommand();
+#else
+            await using var command = connectionToMaster.CreateCommand();
+#endif
+            command.CommandType = CommandType.Text;
+            command.CommandText = $@"
+IF db_id('{databaseName}') IS NOT NULL
+DROP DATABASE {databaseName};
+
+CREATE DATABASE {databaseName};
+";
+            var result = await command.ExecuteNonQueryAsync(cancellationToken);
+            return result > 0;
+        }
+
+        /// <summary>
+        /// Executes a T-SQL command (non-query) that drops a database if it exists.
+        /// </summary>
+        /// <param name="connectionToMaster">
+        /// The SQL connection that will be used to execute the command.
+        /// It must target the master database of a SQL server.
+        /// </param>
+        /// <param name="databaseName">The name of the target database.</param>
+        /// <param name="cancellationToken">The cancellation instruction (optional).</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="connectionToMaster"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="databaseName"/> is the default instance.</exception>
+        /// <exception cref="SqlException">Thrown when the command fails to execute.</exception>
+        public static async Task<bool> TryDropDatabaseAsync(this SqlConnection connectionToMaster, DatabaseName databaseName, CancellationToken cancellationToken = default)
+        {
+            connectionToMaster.MustNotBeNull(nameof(connectionToMaster));
+            databaseName.MustNotBeDefault(nameof(databaseName));
+
+#if NETSTANDARD2_0
+            using var command = connectionToMaster.CreateCommand();
+#else
+            await using var command = connectionToMaster.CreateCommand();
+#endif
+            command.CommandType = CommandType.Text;
+            command.CommandText = $@"
+IF db_id('{databaseName}') IS NOT NULL
+DROP DATABASE {databaseName};
+";
+            var result = await command.ExecuteNonQueryAsync(cancellationToken);
+            return result > 0;
+        }
+
+        /// <summary>
+        /// Executes a T-SQL command (non-query) that creates a database if it does not exist.
+        /// </summary>
+        /// <param name="connectionToMaster">
+        /// The SQL connection that will be used to execute the command.
+        /// It must target the master database of a SQL server.
+        /// </param>
+        /// <param name="databaseName">The name of the target database.</param>
+        /// <param name="cancellationToken">The cancellation instruction (optional).</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="connectionToMaster"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="databaseName"/> is the default instance.</exception>
+        /// <exception cref="SqlException">Thrown when the command fails to execute.</exception>
+        public static async Task<bool> TryCreateDatabaseAsync(this SqlConnection connectionToMaster, DatabaseName databaseName, CancellationToken cancellationToken = default)
+        {
+            connectionToMaster.MustNotBeNull(nameof(connectionToMaster));
+            databaseName.MustNotBeDefault(nameof(databaseName));
+
+#if NETSTANDARD2_0
+            using var command = connectionToMaster.CreateCommand();
+#else
+            await using var command = connectionToMaster.CreateCommand();
+#endif
+            command.CommandType = CommandType.Text;
+            command.CommandText = $@"
+IF db_id({databaseName}) IS NULL
+CREATE DATABASE {databaseName};
+";
+
+            var result = await command.ExecuteNonQueryAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
---
+++ Code/Synnotech.MsSqlServer/DatabaseName.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Synnotech.MsSqlServer
+{
+    /// <summary>
+    /// Represents a string value that is a valid SQL Server database identifier. This is
+    /// done by calling <see cref="SqlEscaping.CheckAndNormalizeDatabaseName"/>. The check is done
+    /// according to the rules of https://docs.microsoft.com/en-us/sql/relational-databases/databases/database-identifiers?view=sql-server-ver15#rules-for-regular-identifiers.
+    /// </summary>
+    public readonly struct DatabaseName : IEquatable<DatabaseName>
+    {
+        private readonly string _databaseName;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="DatabaseName" />.
+        /// </summary>
+        /// <param name="databaseName">The name of the database.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="databaseName"/> is null.</exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="databaseName"/> is an empty string or contains only white space, when it has more than 123 characters
+        /// after being trimmed, or when it contains invalid characters.
+        /// </exception>
+        public DatabaseName(string databaseName)
+        {
+            _databaseName = SqlEscaping.CheckAndNormalizeDatabaseName(databaseName);
+        }
+
+        /// <summary>
+        /// Checks if the specified instance is equal to this one.
+        /// </summary>
+        public bool Equals(DatabaseName other) =>
+            _databaseName == other._databaseName;
+
+        /// <summary>
+        /// Checks if the specified object is an instance of <see cref="DatabaseName" />
+        /// and equal to this instance.
+        /// </summary>
+        public override bool Equals(object @object) =>
+            @object is DatabaseName databaseName && Equals(databaseName);
+
+        /// <summary>
+        /// Returns the hash code of the database name.
+        /// </summary>
+        public override int GetHashCode() => _databaseName.GetHashCode();
+
+        /// <summary>
+        /// Returns the database name.
+        /// </summary>
+        public override string ToString() => _databaseName;
+
+        /// <summary>
+        /// Compares two <see cref="DatabaseName"/> instances for equality.
+        /// </summary>
+        public static bool operator ==(DatabaseName x, DatabaseName y) => x.Equals(y);
+
+        /// <summary>
+        /// Compares two <see cref="DatabaseName"/> instances for inequality.
+        /// </summary>
+        public static bool operator !=(DatabaseName x, DatabaseName y) => !x.Equals(y);
+
+        /// <summary>
+        /// Implicitly converts a string to a database name.
+        /// </summary>
+        public static implicit operator DatabaseName(string databaseName) => new (databaseName);
+    }
+}
